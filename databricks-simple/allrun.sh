623b5b089cc41be1f3f78a8c97b72352f396e9ba
Vijay Donthireddy <vdonthireddy@gmail.com>
2025-03-07 23:16:56 -0800

    working copy with min-io, spark and json file

--- databricks-simple/allrun.sh
+++ databricks-simple/allrun.sh
@@ -1,3 +1,18 @@
+# run the following docker container for min-io
+mkdir -p ~/minio/data
+docker run -d \
+   -p 9000:9000 \
+   -p 9001:9001 \
+   --name minio \
+   -v ~/minio/data:/data \
+   -e "MINIO_ROOT_USER=vijay" \
+   -e "MINIO_ROOT_PASSWORD=donthireddy" \
+   quay.io/minio/minio server /data --console-address ":9001"
+
+# for spark run the following code before the spark-submit
 export SPARK_LOCAL_IP="127.0.0.1"
+
+# submit the jar file to spark
 cd ~/code/spark-3.5.5-bin-hadoop3/
 ./bin/spark-submit --class=com.niharsystems.Main /Users/donthireddy/code/mygit/databricks-simple/target/databricks-simple-1.0-SNAPSHOT-jar-with-dependencies.jar
+
--- databricks-simple/src/main/java/com/niharsystems/Main.java
+++ databricks-simple/src/main/java/com/niharsystems/Main.java
@@ -1,4 +1,7 @@
 package com.niharsystems;
+import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -8,13 +11,14 @@ public class Main {
     public static void main(String[] args) {
         // Create a SparkSession
         SparkSession spark = SparkSession.builder()
+                .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
                 .appName("Simple Spark Example")
                 .master("local[*]")
                 .getOrCreate();
 
         // Create a JavaSparkContext from SparkSession
         JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
-
+/*
         // Create a sample RDD with some text
         JavaRDD<String> textRDD = sc.parallelize(Arrays.asList(
                 "Hello Spark",
@@ -28,8 +32,21 @@ public class Main {
 
         // Print each word
         words.collect().forEach(System.out::println);
+*/
 
-        // Stop the SparkSession
+        sc.hadoopConfiguration().set("fs.s3a.access.key", "2nZqqHPWEzu9JooKNoXO");
+        sc.hadoopConfiguration().set("fs.s3a.secret.key", "DfFaWePTJsp5mB50pS2a7Iz00A6AgJEmdXWGyIOx");
+        sc.hadoopConfiguration().set("fs.s3a.endpoint", "http://localhost:9000");
+        sc.hadoopConfiguration().set("fs.s3a.path.style.access", "true");
+        sc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false");
+        sc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem");
+        sc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false");
+
+        Dataset<Row> df = spark.read().option("multiline", "true").json("s3a://vj-bucket/sample.json");
+        df.printSchema();
+        df.show();
+        System.out.println(df.count());
+        System.out.println(df.columns());
         spark.stop();
     }
 }
